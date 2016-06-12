8eef52f5adb636159c58a54bd4b278c01c3994b8
Lafsody <kirk.kirby@hotmail.com>
2016-06-12 16:24:45 +0700

    - Move project directory - Add Class Character and character controller with initial setup

---
+++ Assets/Scripts/Logic/GameCharacter.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCharacter {
+
+    public enum CHARACTER_TYPE
+    {
+        RED = 0,
+        GREEN = 1,
+        BLUE = 2
+    }
+
+    protected int x;
+    protected int y;
+
+    protected int maxHeart;
+    protected int heart;
+    protected int sword;
+    protected int shield;
+    protected CHARACTER_TYPE characterType;
+
+    public int GetX()
+    {
+        return x;
+    }
+
+    public int GetY()
+    {
+        return y;
+    }
+
+    public int GetHeart()
+    {
+        return heart;
+    }
+
+    public int GetSword()
+    {
+        return sword;
+    }
+
+    public int GetShield()
+    {
+        return shield;
+    }
+
+    public CHARACTER_TYPE GetCharacterType()
+    {
+        return characterType;
+    }
+}
---
+++ Assets/Scripts/Render/GameCharacterController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCharacterController : MonoBehaviour {
+
+    protected float targetX;
+    protected float targetY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void MoveTo(float x, float y)
+    {
+        targetX = x;
+        targetY = y;
+    }
+}
